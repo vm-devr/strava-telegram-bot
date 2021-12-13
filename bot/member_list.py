39a03b1d8f0b1144db8f106e7e863bdbec355aa7
Oleksandr Redko <oleksandr.red+github@gmail.com>
2021-12-13 11:37:38 +0200

    Implement Member.__str__; comment logo usage

--- bot/member_list.py
+++ bot/member_list.py
@@ -3,20 +3,22 @@ from typing import List
 
 
 @dataclass
-class Member(object):
+class Member:
     id_: int
     name: str
 
     def __lt__(self, other) -> bool:
         return self.name.lower() < other.name.lower()
 
+    def __str__(self):
+        return f'<a href="https://www.strava.com/athletes/{str(self.id_)}">{self.name}</a>'
 
-class MemberList(object):
+
+class MemberList:
     @staticmethod
     def printable(member_ids: List[Member]) -> List[str]:
+        # The logo is needed for Telegram's preview. Because if it's not provided Telegram generates preview from
+        # the picture of the first member.
         title = ['Бігуни <a href="https://gutsulrunning.club/images/2021/08/05/header_large.jpeg">GRC:</a>']
-        body = [
-            f'{i:2d}. <a href="https://www.strava.com/athletes/{str(m.id_)}">{m.name}</a>'
-            for m, i in zip(sorted(member_ids), range(1, 1 + len(member_ids)))
-        ]
+        body = [f"{i:2d}. {m}" for m, i in zip(sorted(member_ids), range(1, 1 + len(member_ids)))]
         return title + body
