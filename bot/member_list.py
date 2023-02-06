b8c736b7680f356099d92fa72b5125b46f1fce1c
Oleksandr Redko <oleksandr.red+github@gmail.com>
2023-02-06 14:41:06 +0200

    Adapt output for members > 99

--- bot/member_list.py
+++ bot/member_list.py
@@ -20,5 +20,5 @@ class MemberList:
         # The logo is needed for Telegram's preview. Because if it's not provided Telegram generates preview from
         # the picture of the first member.
         title = ['Бігуни <a href="https://gutsulrunning.club/images/2021/08/05/header_large.jpeg">GRC:</a>']
-        body = [f"{i:2d}. {m}" for m, i in zip(sorted(member_ids), range(1, 1 + len(member_ids)))]
+        body = [f"{i:3d}. {m}" for m, i in zip(sorted(member_ids), range(1, 1 + len(member_ids)))]
         return title + body
--- bot/test/test_member_list.py
+++ bot/test/test_member_list.py
@@ -6,6 +6,6 @@ def test_get_table():
 
     assert MemberList.get_table(members) == [
         'Бігуни <a href="https://gutsulrunning.club/images/2021/08/05/header_large.jpeg">GRC:</a>',
-        ' 1. <a href="https://www.strava.com/athletes/2">John Doe</a>',
-        ' 2. <a href="https://www.strava.com/athletes/1">Вася Пупкін</a>',
+        '  1. <a href="https://www.strava.com/athletes/2">John Doe</a>',
+        '  2. <a href="https://www.strava.com/athletes/1">Вася Пупкін</a>',
     ]
